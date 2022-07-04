2ce24fd43261baf10681190caae4d6a5f65d2462
alexMstar1 <aliaksei.kavalionak@innowise-group.com>
2022-07-04 16:54:10 +0300

    fully adaptive sass implementation

--- app/scripts/core.js
+++ app/scripts/core.js
@@ -1,9 +1,11 @@
+// Video controls onclick
 let video = document.querySelector('.video__src')
 
 video.onclick = () => {
     video.setAttribute('controls', 'controls')
 }
 
+// FAQ accordion activation
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -13,6 +15,7 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
+// Download button scroll to the end
 const scrollToEnd = (selector) => {
   document.querySelector(selector).scrollIntoView({block: "end", behavior: "smooth"})
 }
@@ -22,3 +25,32 @@ let downloadBtn = document.querySelector('#toDownload')
 downloadBtn.onclick = () => {
   scrollToEnd('.download')
 }
+
+
+// Burger menu sidebas GSAP
+var burgerTop= document.getElementsByClassName("top");
+var burgerBot= document.getElementsByClassName("bottom");
+var burgerMid= document.getElementsByClassName("middle");
+var burgerSidebar= document.getElementsByClassName("sidebar");
+var sideText= document.getElementsByClassName("sidetext");
+var burgerWhole= document.querySelectorAll(".top, .bottom, .middle");
+
+var tl = new TimelineMax({paused:true, reversed:true});
+tl.timeScale(2);
+tl
+.to(burgerTop, 0.7, { y: 11, yoyo: true, ease: Power1.easeInOut})
+.to(burgerBot, 0.7, { y: -11, yoyo: true, ease: Power1.easeInOut}, '-=0.7')
+.to(burgerTop, 1, {rotation:45})
+.to(burgerMid, 1, {rotation:45}, '-=1')
+.to(burgerBot, 1, {rotation:135}, '-=1')
+.to(burgerWhole, 0.1, {css:{borderColor:"#000"}, ease: Power1.easeOut}, '-=1')
+.to(burgerSidebar, 1, {x:-300, ease: Power2.easeOut}, '-=1')
+.staggerFrom(sideText, 0.5, {opacity: 0, y: 25, ease: Power1.easeOut}, 0.1, "-=0.7")
+.to(sideText, 0.2, {css:{color:"#000"}})
+.reverse()
+
+document.querySelector('.burger-click-region').addEventListener('click', () => {
+  tl.reversed() ? tl.play() : tl.reverse()
+})
+
+
